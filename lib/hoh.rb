a514d89f93fb7412c7e3f5f6538a6f2f66ae3ca9
Kyle Talmage <ktalmage85@gmail.com>
2020-02-10 14:10:52 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,11 +31,11 @@
  }
 
 def naming_system
-
+hoh1 ={}
   #So, if we have the "Kingdom" node we should be able to "tunnel" into the
   # HoH all the way to Species!
 hoh = [PORTION_1,PORTION_2,PORTION_3,PORTION_4[:label]]
-
+hoh1 << hoh
 end
 puts naming_system
 
